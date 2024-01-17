9ea26ab9b6ec2e49a83962a47e2a88f3ac77e858
damian-nestea <damian@hotmail.cz>
2024-01-17 06:00:11 -0300

    feat criação de arquivo e estilo global com styled components

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import Header from "./Components/Header";
 import Home from "./Pages/Home";
 import Footer from "./Components/Footer";
+import GlobalStyle from "./globalStyles";
 
 function App() {
   return (
-    <div>
+    <>
+      <GlobalStyle />
       <Header />
       <Home />
       <Footer />
-    </div>
+    </>
   );
 }
 
