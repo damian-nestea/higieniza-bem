95b92380badd4f38a19e35f4339e6614a4496fe0
damian-nestea <damian@hotmail.cz>
2024-01-20 10:13:37 -0300

    feat adição de imagem no topo da home page com texto, estilização básica

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -2,7 +2,19 @@ import React from "react";
 import { Container } from "./styles";
 
 const Home = () => {
-  return <Container>Home</Container>;
+  return (
+    <Container>
+      <div>
+        <img
+          src="https://revistacarro.com.br/wp-content/uploads/2022/03/Abarth-Pulse_3.jpg"
+          alt="Imagem de Carro"
+        />
+        <p>
+          BEM-VINDO À HIGIENIZA BEM! <span>Conheça nossos serviços...</span>
+        </p>
+      </div>
+    </Container>
+  );
 };
 
 export default Home;
