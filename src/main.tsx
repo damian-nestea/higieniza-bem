53a5e3e845feeb76c9ed19cb2c12d557f5893dae
damian-nestea <damian@hotmail.cz>
2024-01-22 19:09:59 -0300

    chore import de módulos necessários para estilização do carrossel com swiper para carregar na raiz do projeto

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes.tsx";
 
+import { register } from "swiper/element/bundle";
+
+register();
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
