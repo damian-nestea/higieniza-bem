48ad1b8a3c578010e3367688222b0087f2b6ba40
damian-nestea <damian@hotmail.cz>
2024-01-25 20:25:39 -0300

    feat criação e estilização básica de card de diferenciais da empresa

--- src/Components/Advantages/AdvantageCard/index.tsx
+++ src/Components/Advantages/AdvantageCard/index.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import { Container } from "./styles";
 
-const AdvantageCard = () => {
-  return <Container>AdvantageCard</Container>;
+export type AdvantageProps = {
+  id: number;
+  icon: string;
+  description: string;
+};
+
+const AdvantageCard = ({ id, icon, description }: AdvantageProps) => {
+  return (
+    <Container key={id}>
+      <img src={icon} alt="Icone" />
+      <p>{description}</p>
+    </Container>
+  );
 };
 
 export default AdvantageCard;
--- src/Components/Advantages/AdvantageCard/styles.ts
+++ src/Components/Advantages/AdvantageCard/styles.ts
@@ -1,5 +1,23 @@
 import styled from "styled-components";
 
 export const Container = styled.article`
-  width: 100%;
+  width: 80%;
+
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  gap: 1rem;
+
+  border: 1px solid #aaa;
+  padding: 2rem;
+  border-radius: 0.5rem;
+
+  :not(:last-child) {
+    margin-bottom: 2rem;
+  }
+
+  img {
+    width: 50%;
+    border-radius: 50%;
+  }
 `;
